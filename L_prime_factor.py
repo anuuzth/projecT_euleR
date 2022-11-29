4fee6a3a35b21dd13c034a253fddd7673b6aa6d8
anuj <anuuzth@gmail.com>
2022-11-29 16:35:57 +0545

    new solutions committed for finding largest prime factor and pallindrome of product of two 3 digit numbers.

---
+++ L_prime_factor.py
@@ -0,0 +1,23 @@
+def prime(n):
+    divisor = 2
+    array = []
+    while divisor <= n:
+        if n % divisor == 0:
+            array.append(divisor)
+            n = n/divisor
+        else:
+            divisor += 1
+            
+    return(array)
+
+def largest_primefactor():
+    n = int(input("enter the number to find largest prime_factor of:"))
+    a = prime(n)
+    largest = a[0]
+    for element in a:
+        if element > largest:
+            largest = element
+    print(largest)
+
+
+largest_primefactor()
---
+++ largest_palindrom3digit.py
@@ -0,0 +1,26 @@
+def reverse(n):
+    rev = 0
+    while n > 0:
+        rev = rev * 10 + n % 10
+        n = n // 10
+    return(rev)
+
+def pallindrome():
+    a = []
+    for i in range(100,1000):
+        for j in range(100,1000):
+            x = i*j 
+            if x == reverse(x):
+                a.append(x)
+    return(a)
+def largest_pallindrome():
+    a = pallindrome()
+    largest = a[0]
+    for element in a:
+        if element > largest:
+            largest = element
+    print(largest)
+        
+
+largest_pallindrome()
+
